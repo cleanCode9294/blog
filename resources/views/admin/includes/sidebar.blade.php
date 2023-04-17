b1c58b88a18b0d34b6c116bbc17c0f894c5b6edf
Golib <gxayrullayev@gmail.com>
2023-04-17 17:53:24 +0500

    Added breadcrumbs

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -9,6 +9,12 @@
             <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
                 <li class="nav-item">
+                    <a href="{{ route('admin.main.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="{{ route('admin.user.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-user-alt"></i>
                         <p>Пользователи</p>
